727885e5db0ed5a416ea833e13c9e3495f701691
plank4 <161429026+plank4@users.noreply.github.com>
2024-03-27 14:09:50 +0800

    lab5

--- src/ControllerLab5.c
+++ src/ControllerLab5.c
@@ -8,9 +8,9 @@
 //static function prototypes, functions only called in this file
 
 //Global initialisation
-uint32_t time_ms = 0;
-uint8_t time_s = 0;
-uint8_t time_m = 0;
+volatile uint32_t time_ms = 0;
+volatile uint8_t time_s = 0;
+volatile uint8_t time_m = 0;
 
 int main(void)
 {
@@ -25,11 +25,12 @@ int main(void)
   cli(); //DISABLE INTERRUPTS
   TCCR1A = 0;
   TCCR1B |= (1<<WGM12)|(1<<WGM13)|(1<<CS10); //SET INTERRUPT ON CAPTURE FLAG / PRESCALER 1
+  //WGM13, WGM12 set CTC and timer sequencing
   TCNT1 = 0;
   ICR1 = 15999; //SET TOP VALUE / (0.001*16000000/1)-1
   TIMSK1 = (1<<ICIE1); //INPUT CAPTURE ENABLE
   //Button interrupt setup
-  DDRD &= ~(1<<DD0)&(1<<DD1); //INT0 INT1 INT2 INPUT MODE
+  DDRD &= ~(1<<DD0)&(1<<DD1); //INT0 INT1 INPUT MODE
   PORTD |= (1<<PD0)|(1<<PD1); //ENABLE PULLUPS
   EICRA |= (1<<ISC01)|(1<<ISC11); 
   EICRA &= ~(1<<ISC00)&(1<<ISC10); //FALLING EDGE TRIGGER
@@ -49,7 +50,7 @@ int main(void)
       time_ms -= 1000;
       time_s += 1;
     }
-    sprintf(serial_string, "%d m &d s &d ms", time_m, time_s, time_ms); //FORMAT TIME VARIABLES INTO STRING
+    sprintf(serial_string, "%d m %d s %d ms", time_m, time_s, time_ms); //FORMAT TIME VARIABLES INTO STRING
     serial0_print_string(serial_string); //PRINT STRING
   }return(1);
 }//end main 
@@ -62,7 +63,7 @@ ISR(INT0_vect){//BUTTON 0
 uint32_t time0 = milliseconds_now(); //EXPERIMENTAL BETTER TRIGGER METHOD
 static uint32_t prevTime0 = 0;
 if(time0 > (prevTime0 + DEBOUNCE_PERIOD)){
-  PORTA ^= (1<<PA0); //TOGGLE LED
+  PORTA ^= (1<<PC0); //TOGGLE LED
   prevTime0 = time0;
   TIMSK1 ^= 1;
 }
@@ -74,7 +75,7 @@ ISR(INT1_vect){//BUTTON 1
 uint32_t time1 = milliseconds_now(); //EXPERIMENTAL BETTER TRIGGER METHOD
 static uint32_t prevTime1 = 0;
 if(time1 > (prevTime1 + DEBOUNCE_PERIOD)){
-  PORTA ^= (1<<PA1); //TOGGLE LED
+  PORTA ^= (1<<PC1); //TOGGLE LED
   prevTime1 = time1;
   time_ms = 0, time_s = 0, time_m = 0;
   
